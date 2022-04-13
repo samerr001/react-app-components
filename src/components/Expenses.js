463dd63f38e089889b0a3292dd8b02530ccc9892
Samer Mefteh <samer.mefteh@gmail.com>
2022-04-14 01:18:25 +0200

    Example about props.children

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,12 +1,13 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.expenses.map((item) => (
         <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
       ))}
-    </div>
+    </Card>
   );
 }
 
