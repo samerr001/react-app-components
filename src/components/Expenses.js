b70a4ee949b258258428003f1a05b93d0cd6dde1
Samer Mefteh <samer.mefteh@gmail.com>
2022-04-14 00:59:21 +0200

    building different components

---
+++ src/components/Expenses.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      {props.expenses.map((item) => (
+        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+      ))}
+    </div>
+  );
+}
+
+export default Expenses;
